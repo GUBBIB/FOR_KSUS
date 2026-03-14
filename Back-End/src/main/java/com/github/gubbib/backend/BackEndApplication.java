15079e7319682311791e1f9ecbc7c7eb24417250
GUBBIB <moon.dragon250@gmail.com>
2026-03-15 04:11:22 +0900

    JPA Auditing 및 Method Security 설정 ã추가

--- Back-End/src/main/java/com/github/gubbib/backend/BackEndApplication.java
+++ Back-End/src/main/java/com/github/gubbib/backend/BackEndApplication.java
@@ -2,8 +2,12 @@ package com.github.gubbib.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableMethodSecurity
 public class BackEndApplication {
 
 	public static void main(String[] args) {
