7eb227b96c1b1d0c329f1eb5279285e69421e061
GUBBIB <moon.dragon250@gmail.com>
2026-03-15 17:22:11 +0900

    Base Entity 및 User 도메인 추가

---
+++ Back-End/src/main/java/com/github/gubbib/backend/Domain/User/UserRole.java
@@ -0,0 +1,17 @@
+package com.github.gubbib.backend.Domain.User;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    USER("USER"),
+    ADMIN("ADMIN"),
+    SYSTEM("SYSTEM");
+
+    private final String roleName;
+
+    UserRole(String roleName) {
+        this.roleName = roleName;
+    }
+
+}
