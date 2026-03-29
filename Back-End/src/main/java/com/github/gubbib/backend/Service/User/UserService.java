e0d817da84b3be319233584481a4ea5c232a7b77
GUBBIB <moon.dragon250@gmail.com>
2026-03-29 17:00:00 +0900

    user service 추가

--- Back-End/src/main/java/com/github/gubbib/backend/Service/User/UserService.java
+++ Back-End/src/main/java/com/github/gubbib/backend/Service/User/UserService.java
@@ -1,4 +1,9 @@
 package com.github.gubbib.backend.Service.User;
 
+import com.github.gubbib.backend.Domain.User.User;
+import com.github.gubbib.backend.Principal.CustomUserPrincipal;
+
 public interface UserService {
+    User checkUser(CustomUserPrincipal userPrincipal);
+    User findUser(Long userId);
 }
