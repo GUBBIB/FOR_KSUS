aea018499192c8cacf42fec95a6afa87be3541f1
GUBBIB <moon.dragon250@gmail.com>
2026-04-04 16:33:24 +0900

    Building domain 추가

---
+++ Back-End/src/main/java/com/github/gubbib/backend/Domain/Building/Building.java
@@ -0,0 +1,27 @@
+package com.github.gubbib.backend.Domain.Building;
+
+
+import com.github.gubbib.backend.Domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "buildings")
+public class Building extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+}
