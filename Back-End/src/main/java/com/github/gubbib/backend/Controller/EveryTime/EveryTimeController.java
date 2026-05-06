244d2134697728ecdfedad02609a010020654f08
GUBBIB <moon.dragon250@gmail.com>
2026-05-06 11:37:18 +0900

    건물/강의실 목록 조회 api 추가

--- Back-End/src/main/java/com/github/gubbib/backend/Controller/EveryTime/EveryTimeController.java
+++ Back-End/src/main/java/com/github/gubbib/backend/Controller/EveryTime/EveryTimeController.java
@@ -1,17 +1,21 @@
 package com.github.gubbib.backend.Controller.EveryTime;
 
+import com.github.gubbib.backend.DTO.Building.BuildingResponseDTO;
+import com.github.gubbib.backend.DTO.ClassRoom.ClassRoomResponseDTO;
 import com.github.gubbib.backend.DTO.EveryTime.FetchRequestDTO;
+import com.github.gubbib.backend.Service.Building.BuildingService;
+import com.github.gubbib.backend.Service.Building.BuildingServiceImp;
+import com.github.gubbib.backend.Service.ClassRoom.ClassRoomService;
 import com.github.gubbib.backend.Service.EveryTime.TimetableService;
+import com.github.gubbib.backend.Service.EveryTime.TimetableServiceImp;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -20,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class EveryTimeController {
 
     private final TimetableService timetableService;
+    private final BuildingService buildingService;
+    private final ClassRoomService classRoomService;
 
     @PostMapping("/fetch")
     @PreAuthorize("hasRole('ADMIN')")
@@ -27,8 +33,21 @@ public class EveryTimeController {
             @RequestBody FetchRequestDTO request
     ) throws Exception {
 
-        timetableService.fetchAndSaveClassrooms(request.cookie());
+        timetableService.fetchAndSaveTimetable(request.cookie());
 
         return ResponseEntity.ok("강의실 데이터 수집 완료");
+    }
+
+    @GetMapping("/buildings")
+    public List<BuildingResponseDTO> getBuildings(){
+
+        return buildingService.getAllBuildings();
+    }
+
+    @GetMapping("/buildings/{buildingID}/classrooms")
+    public List<ClassRoomResponseDTO> getClassRooms(
+            @PathVariable Long buildingID
+    ){
+        return classRoomService.getClassRoomsByBuilding(buildingID);
     }
 }
