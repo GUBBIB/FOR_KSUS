93d1a5f811565a2d08bab8b1c9edc4d5e4f69a54
GUBBIB <moon.dragon250@gmail.com>
2026-05-14 11:24:46 +0900

    EveryTime DTO - SubjectDTO 수정

--- Back-End/src/main/java/com/github/gubbib/backend/DTO/EveryTime/SubjectDTO.java
+++ Back-End/src/main/java/com/github/gubbib/backend/DTO/EveryTime/SubjectDTO.java
@@ -1,18 +1,28 @@
 package com.github.gubbib.backend.DTO.EveryTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.dataformat.xml.annotation.*;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.github.gubbib.backend.DTO.EveryTime.TimePlaceDTO;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-
 public class SubjectDTO {
 
     @JacksonXmlProperty(isAttribute = true)
+    private String code;
+
+    @JacksonXmlProperty(isAttribute = true)
     private String name;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String professor;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String lectureId;
 
     @JacksonXmlElementWrapper(useWrapping = false)
     @JacksonXmlProperty(localName = "timeplace")
