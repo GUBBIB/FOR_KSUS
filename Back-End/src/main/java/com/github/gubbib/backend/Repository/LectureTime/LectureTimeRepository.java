7cb05ec56aa5d3dd95e1dec51e4d73ab3cfe0067
GUBBIB <moon.dragon250@gmail.com>
2026-05-19 10:16:54 +0900

    LectureTime Repository 추가

--- Back-End/src/main/java/com/github/gubbib/backend/Repository/LectureTime/LectureTimeRepository.java
+++ Back-End/src/main/java/com/github/gubbib/backend/Repository/LectureTime/LectureTimeRepository.java
@@ -4,7 +4,10 @@ import com.github.gubbib.backend.Domain.ClassRoom.ClassRoom;
 import com.github.gubbib.backend.Domain.LectureTime.LectureTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LectureTimeRepository extends JpaRepository<LectureTime, Long> {
 
     boolean existsByClassRoomAndDayAndStartAndEnd(ClassRoom classRoom, int day, int start, int end);
+    List<LectureTime> findByClassRoomId(Long classroomId);
 }
