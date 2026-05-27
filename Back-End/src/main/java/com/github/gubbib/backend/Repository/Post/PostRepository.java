bcb300d5dd1351ea77a64d436c2b36b680fd2c85
GUBBIB <moon.dragon250@gmail.com>
2026-05-27 14:03:31 +0900

    게시글 조회 방식 변경

--- Back-End/src/main/java/com/github/gubbib/backend/Repository/Post/PostRepository.java
+++ Back-End/src/main/java/com/github/gubbib/backend/Repository/Post/PostRepository.java
@@ -4,8 +4,10 @@ import com.github.gubbib.backend.Domain.Post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findByBoardIdOrderByCreatedAtDesc(Long boardId);
+    List<Post> findByBoardIdAndIsDeletedFalseOrderByCreatedAtDesc(Long boardId);
+    Optional<Post> findByIdAndBoardIdAndIsDeletedFalse(Long postId, Long boardId);
 }
