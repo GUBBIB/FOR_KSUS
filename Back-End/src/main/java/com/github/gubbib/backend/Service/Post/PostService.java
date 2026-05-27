cf3e55b5aecdbcc890007592487752414a226a0b
GUBBIB <moon.dragon250@gmail.com>
2026-05-27 14:03:05 +0900

    조회, 삭제, 수정 api 추가

--- Back-End/src/main/java/com/github/gubbib/backend/Service/Post/PostService.java
+++ Back-End/src/main/java/com/github/gubbib/backend/Service/Post/PostService.java
@@ -2,12 +2,18 @@ package com.github.gubbib.backend.Service.Post;
 
 import com.github.gubbib.backend.DTO.Post.PostCreateRequestDTO;
 import com.github.gubbib.backend.DTO.Post.PostResponseDTO;
+import com.github.gubbib.backend.DTO.Post.PostUpdateRequestDTO;
+import com.github.gubbib.backend.Domain.Post.Post;
 import com.github.gubbib.backend.Principal.CustomUserPrincipal;
 
 import java.util.List;
 
 public interface PostService {
 
+    Post existsPost(Long boardId, Long postId);
     PostResponseDTO createPost(Long boardId, PostCreateRequestDTO request, CustomUserPrincipal userPrincipal);
     List<PostResponseDTO> getAllPosts(Long boardId);
+    PostResponseDTO getPost(Long boardId, Long postId);
+    PostResponseDTO updatePost(Long boardId, Long postId, PostUpdateRequestDTO request, CustomUserPrincipal userPrincipal);
+    void deletePost(Long boardId, Long postId, CustomUserPrincipal userPrincipal);
 }
--- Back-End/src/main/java/com/github/gubbib/backend/Service/Post/PostServiceImp.java
+++ Back-End/src/main/java/com/github/gubbib/backend/Service/Post/PostServiceImp.java
@@ -2,8 +2,11 @@ package com.github.gubbib.backend.Service.Post;
 
 import com.github.gubbib.backend.DTO.Post.PostCreateRequestDTO;
 import com.github.gubbib.backend.DTO.Post.PostResponseDTO;
+import com.github.gubbib.backend.DTO.Post.PostUpdateRequestDTO;
 import com.github.gubbib.backend.Domain.Board.Board;
 import com.github.gubbib.backend.Domain.Post.Post;
+import com.github.gubbib.backend.Exception.ErrorCode;
+import com.github.gubbib.backend.Exception.GlobalException;
 import com.github.gubbib.backend.Principal.CustomUserPrincipal;
 import com.github.gubbib.backend.Repository.Post.PostRepository;
 import com.github.gubbib.backend.Service.Board.BoardService;
@@ -22,6 +25,12 @@ public class PostServiceImp implements PostService{
 
     private final BoardService boardService;
     private final PostRepository postRepository;
+
+    @Override
+    public Post existsPost(Long boardId, Long postId) {
+        return postRepository.findByIdAndBoardIdAndIsDeletedFalse(postId, boardId)
+                .orElseThrow(() -> new GlobalException(ErrorCode.POST_NOT_FOUND));
+    }
 
     @Override
     @Transactional
@@ -46,9 +55,54 @@ public class PostServiceImp implements PostService{
 
         Board b = boardService.existsBoard(boardId);
 
-        return postRepository.findByBoardIdOrderByCreatedAtDesc(boardId)
+        return postRepository.findByBoardIdAndIsDeletedFalseOrderByCreatedAtDesc(boardId)
                 .stream()
                 .map(PostResponseDTO::from)
                 .toList();
+    }
+
+    @Override
+    public PostResponseDTO getPost(Long boardId, Long postId) {
+
+        Board b = boardService.existsBoard(boardId);
+
+        // 조회수 증가 로직 추가 필요
+        Post p = postRepository.findByIdAndBoardIdAndIsDeletedFalse(postId, b.getId())
+                .orElseThrow(() -> new GlobalException(ErrorCode.POST_NOT_FOUND));
+
+        return  PostResponseDTO.from(p);
+    }
+
+    @Override
+    @Transactional
+    public PostResponseDTO updatePost(Long boardId, Long postId, PostUpdateRequestDTO request, CustomUserPrincipal userPrincipal) {
+
+        Board b = boardService.existsBoard(boardId);
+        Post p = existsPost(postId, b.getId());
+
+        if(!p.getUser().getId().equals(userPrincipal.getUser().getId())){
+            throw new GlobalException(ErrorCode.POST_UPDATE_FORBIDDEN);
+        }
+
+        if(!p.getTitle().equals(request.title()) || !p.getContent().equals(request.content())) {
+            p.updatePost(request.title(), request.content());
+        } else {
+            throw new GlobalException(ErrorCode.POST_NO_CHANGES);
+        }
+
+        return PostResponseDTO.from(p);
+    }
+
+    @Override
+    @Transactional
+    public void deletePost(Long boardId, Long postId, CustomUserPrincipal userPrincipal) {
+        Board b = boardService.existsBoard(boardId);
+        Post p = existsPost(postId, b.getId());
+
+        if(!p.getUser().getId().equals(userPrincipal.getUser().getId())){
+            throw new GlobalException(ErrorCode.POST_DELETE_FORBIDDEN);
+        }
+
+        p.changeIsDeleted();
     }
 }
