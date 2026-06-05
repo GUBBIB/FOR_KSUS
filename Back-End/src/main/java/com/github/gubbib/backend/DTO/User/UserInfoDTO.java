1e9c8830b363d4b5bad2f65151e5200996aab442
GUBBIB <moon.dragon250@gmail.com>
2026-06-05 18:01:31 +0900

    User DTO추가

---
+++ Back-End/src/main/java/com/github/gubbib/backend/DTO/User/UserInfoDTO.java
@@ -0,0 +1,11 @@
+package com.github.gubbib.backend.DTO.User;
+
+import lombok.Builder;
+
+@Builder
+public record UserInfoDTO(
+        String email,
+        String nickname,
+        String name
+) {
+}
